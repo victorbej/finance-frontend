382136350a5d4aa2b392597edb1f79f60d45fa13
Victor Bej <victorbejenar@yandex.ru>
2021-11-23 20:08:38 +0300

    header component fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import './App.scss';
-import '../Header/Header';
 import Header from '../Header/Header';
 function App() {
   return (
-    <>
-    <Header />
-    </>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
