c06cd9d932c4d69b422d7b4f81b91b05c447e032
Victor Bej <victorbejenar@yandex.ru>
2021-11-23 21:41:50 +0300

    footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from '../Header/Header';
 import Tools from '../Tools/Tools';
+import Footer from '../Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Header />
       <Tools />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__author">Made by Victor Bej</p>
+        </footer>
+    );
+}
+
+export default Footer;
