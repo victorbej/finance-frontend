8509f180680e3133c8f6f68ff80d4b53e48b74fb
Victor Bej <victorbejenar@yandex.ru>
2021-11-26 00:05:49 +0300

    link fixed to navlink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import './Header.scss';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
         <header className="header">
             <h1 className="header__title">Финансовый конструктор</h1>
             <div className="header__user">
-                <Link className="header__link" to="/tools">Инструменты</Link>
-                <Link className="header__link" to="/login">Войти</Link>
-                <Link className="header__link" to="/registration">Регистрация</Link>
+                <NavLink activeClassName="active" className="header__link" to="/tools">Инструменты</NavLink>
+                <NavLink activeClassName="active" className="header__link" to="/login">Войти</NavLink>
+                <NavLink activeClassName="active" className="header__link" to="/registration">Регистрация</NavLink>
             </div>
         </header>
     );
