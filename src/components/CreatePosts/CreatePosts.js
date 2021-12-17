4dbc86dd0230e95a7365bc6a013a87be6ad250c4
Victor Bej <victorbejenar@yandex.ru>
2021-12-17 21:31:59 +0300

    blog component changed

--- src/components/CreatePosts/CreatePosts.js
+++ src/components/CreatePosts/CreatePosts.js
@@ -4,7 +4,7 @@ import PostForm from './PostForm';
 
 import classes from './Posts.module.scss';
 
-function CreatePosts() {
+export default function CreatePosts() {
     const [posts, setPosts] = React.useState([
         { id: 1, url: 'https://images.unsplash.com/photo-1633311905139-7b6088a69e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80', title: 'Искусственный интеллект', body: 'В этот сектор входят разработки в области машинного обучения, нейросетей, робототехники, распознавания лица и речи. Благодаря нейросетям бортовой компьютер самоуправляемого транспорта может отличить пешехода от прочих объектов на трассе. Инвестировать в искусственный интеллект и робототехнику можно через несколько фондов: IRBO, ROBO, BOTZ, ROBT. Мне понравился глобальный фонд IRBO — iShares Robotics and Artificial Intelligence ETF. Он самый свежий: запущен в августе 2018 года. Стоимость обслуживания значительно ниже, чем у конкурентов, — 0,47%.' }
     ]);
@@ -21,20 +21,13 @@ function CreatePosts() {
         setPosts(newList);
     }
 
-    const pageTitle = ['Создайте свой пост об инвестициях'];
-
     return (
-        <div className="Posts">
-            <h2 className={classes.pageTitle}>{pageTitle}</h2>
-            <div className="container">
-                {posts.length !== 0
-                    ? <PostLists remove={remove} posts={posts} />
-                    : <h2 className={classes.empty}>Посты не найдены, их необходимо создать...</h2>
-                }
-                <PostForm create={createPost} />
-            </div>
+        <div className="container">
+            {posts.length !== 0
+                ? <PostLists remove={remove} posts={posts} />
+                : <h2 className={classes.empty}>Посты не найдены, их необходимо создать...</h2>
+            }
+            <PostForm create={createPost} />
         </div>
-    )
+    );
 }
-
-export default CreatePosts;
--- src/components/CreatePosts/PostLists.js
+++ src/components/CreatePosts/PostLists.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import DeleteIcon from '@mui/icons-material/Delete';
-import CountLikes from './CountLikes';
 
-import classes from './Posts.module.scss';
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 function PostLists(props) {
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     return (
         <ul>
             {props.posts.map((post, index) =>
                 <li key={index}>
-                    <h2 className={classes.postTitle}>{post.title}</h2>
-                    <img className={classes.postImage} src={post.url} alt={post.title} />
-                    <p className={classes.postBody}>{post.body}</p>
-                    <div className={classes.container}>
-                        <Button variant="contained" className={classes.deleteButton} startIcon={<DeleteIcon />} onClick={() => props.remove(post)}>Удалить пост</Button>
-                        <CountLikes />
-                    </div>
-
+                    <Card sx={{ maxWidth: '100vh', margin: 'auto' }}>
+                        <CardHeader
+                            avatar={
+                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                    A
+                                </Avatar>
+                            }
+                            action={
+                                <IconButton onClick={() => props.remove(post)} aria-label="settings">
+                                    <DeleteIcon />
+                                </IconButton>
+                            }
+                            title={post.title}
+                            subheader="Сентябрь 21, 2021"
+                        />
+                        <CardMedia
+                            component="img"
+                            height="250"
+                            image={post.url}
+                            alt={post.title}
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                                Скоро здесь будет дополнительный текст
+                            </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="add to favorites">
+                                <FavoriteIcon />
+                            </IconButton>
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </ExpandMore>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Typography paragraph>
+                                    {post.body}
+                                </Typography>
+                            </CardContent>
+                        </Collapse>
+                    </Card>
                 </li>
             )}
         </ul>
