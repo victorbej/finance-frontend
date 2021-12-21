f3fef1f26df0d8452199c0a711d1d8d3505e5c78
Victor Bej <victorbejenar@yandex.ru>
2021-12-21 11:31:53 +0300

    calculator component started

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,10 +1,44 @@
-import './Calculator.scss';
+import React from "react";
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import classes from './Calculator.module.scss';
+
+const theme = createTheme({
+    palette: {
+        background: { default: "#161616" },
+        text: { primary: "rgb(255, 255, 255)", secondary: "rgba(255, 255, 255, .5)" }
+    },
+});
 
 function Calculator() {
+
+    const [savings, setSavings] = React.useState([1600000]);
+
+    function changeSavings(e) {
+        setSavings(e.target.value);
+    }
+
+    function onlyNumbers(e) {
+        if (!/[0-9]/.test(e.key)) {
+            e.preventDefault();
+        }
+    }
+
     return (
-        <div>
-            <p>Calculator</p>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className={classes.container}>
+                <div className={classes.wrapper}>
+                    <h2 className={classes.title}>Первоначальный взнос</h2>
+                    <p className={classes.body}>Прежде чем взять ипотеку на квартиру,
+                        нужно накопить на первоначальный взнос —
+                        обычно 15% от стоимости жилья.</p>
+                    <div className={classes.input}>
+                        <TextField id="outlined-basic" label="Накопления" variant="outlined" value={savings} onChange={changeSavings} onKeyPress={onlyNumbers} type="text" />
+                        <p>₽</p>
+                    </div>
+                </div>
+            </div>
+        </ThemeProvider>
     );
 }
 
